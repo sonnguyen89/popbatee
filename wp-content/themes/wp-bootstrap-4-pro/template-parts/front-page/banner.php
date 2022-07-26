a06fc454f5a4f36a3549971223ebb00bbf8f7d7f
Son Nguyen <nam_son14@yahoo.com>
2022-07-26 23:42:41 +1000

    update Homepage template
    inject ACF to template

--- wp-content/themes/wp-bootstrap-4-pro/template-parts/front-page/banner.php
+++ wp-content/themes/wp-bootstrap-4-pro/template-parts/front-page/banner.php
@@ -1,14 +1,14 @@
 <section class="jumbotron text-center wp-bs-4-jumbotron border-bottom text-white">
     <div class="wp-bp-jumbo-overlay">
         <div class="container-fluid banner-container-fluid p-0">
+            <?php $banners = get_field( "banners" ); ?>
             <div id="banner-carousel" class="carousel slide" data-ride="carousel">
                 <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img class="d-block w-100" src="http://popbatee.test/wp-content/uploads/2022/07/banner.png" alt="banner"/>
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block w-100" src="http://popbatee.test/wp-content/uploads/2022/07/banner.png" alt="banner">
-                    </div>
+                    <?php foreach($banners as $index => $banner_item):  ?>
+                        <div class="carousel-item <?php if ($index == 0): ?> active <?php endif; ?>">
+                            <img class="d-block w-100" src="<?php echo $banner_item['image'] ?>" alt="banner"/>
+                        </div>
+                    <?php endforeach; ?>
                 </div>
                 <a class="carousel-control-prev" href="#banner-carousel" data-slide="prev">
                     <span class="carousel-control-prev-icon"></span>
